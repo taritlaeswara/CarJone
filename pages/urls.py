c4d6ebe3249db261e6ca4b913ef27419e5b60415
taritlaeswara <taritlaeswara@gmailcom>
2021-06-12 08:28:14 +0530

    create pages and configure nav

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('services', views.services, name="services"),
+
 ]
